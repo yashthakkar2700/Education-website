0e66dd9bd6a04dd91c67d965c8132aacf5818b57
Yash Sanjay Thakkar <yashthakkar2700@gmail.com>
2020-05-03 21:11:33 +0530

    Made about page, blog-news page, blog-details page, courses page

--- js/script.js
+++ js/script.js
@@ -9,14 +9,13 @@ $(document).ready(function(){
        autoplay: true,
        margin: 20,
        loop: true,
-       nav: false,
+       nav: true,
        smartspeed: 700,
        autoplayHoverPause: true,
        dots: false,
-//       navText: ['<i class="lni-chevron-left-circle"></i>', '<i class="lni-chevron-right-circle"></i>']
+       navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
    });               
 });
-
 
 /************************************
         TESTIMONIAL
@@ -34,5 +33,57 @@ $(document).ready(function(){
 //       navText: ['<i class="lni-chevron-left-circle"></i>', '<i class="lni-chevron-right-circle"></i>']
    });               
 });
+
+/************************************
+        Featured Courses
+**********************************/
+$(document).ready(function(){
+   $("#courses-slider").owlCarousel({
+       items: 3,
+       autoplay: true,
+       margin: 20,
+       loop: true,
+       nav: true,
+       smartspeed: 700,
+       autoplayHoverPause: true,
+       dots: false,
+       navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
+   });               
+});
+
+/************************************
+        Teahers Courses
+**********************************/
+$(document).ready(function(){
+   $("#teachers-slider").owlCarousel({
+       items: 3,
+       autoplay: true,
+       margin: 20,
+       loop: true,
+       nav: true,
+       smartspeed: 700,
+       autoplayHoverPause: true,
+       dots: false,
+       navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
+   });               
+});
+
+/************************************
+        Blog Courses
+**********************************/
+$(document).ready(function(){
+   $("#blog-slider").owlCarousel({
+       items: 3,
+       autoplay: true,
+       margin: 20,
+       loop: true,
+       nav: true,
+       smartspeed: 700,
+       autoplayHoverPause: true,
+       dots: false,
+       navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
+   });               
+});
+
 
 
